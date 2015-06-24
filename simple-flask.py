afa8b85d62120986e4d5091fd25e1c0b27ca5921
Heitor Meira <htrmeira@gmail.com>
2015-06-24 03:13:16 -0300

    changing simple-flask to write and read a simple file

--- simple-flask.py
+++ simple-flask.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ("Hostname: %s" % (socket.gethostname()))
+	with open("/storage/simple-flask-storage/teste.txt", "a+") as somefile:
+		towrite = ("<div>Hostname: %s</div>\n" % (socket.gethostname()) )
+		somefile.write(towrite)
+		somefile.close()
+
+	with open("/storage/simple-flask-storage/teste.txt", "r+") as rofile:
+		return ("<body>%s</body>" % (rofile.read()) )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
